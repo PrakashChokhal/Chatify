ad65eaffdf6852050f7daf2f8274b0666d39bb49
Prakash Chokhal <prakashchokhal01@gmail.com>
2026-08-27 18:41:10 +0530

    -Implement message controller with functions to get users, conversations, and messages -Add route for sending messages with media uploads Introduce middleware for handling file uploads -Update package.json and package-lock.json to include necessary dependency

---
+++ backend/src/routes/message.route.js
@@ -0,0 +1,16 @@
+import express from "express";
+import { getUsersForSidebar ,getConversationsForSidebar, getMessages, sendMessages} from "../controllers/message.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { upload } from "../middleware/upload.middleware.js";
+
+const router = express.Router();
+
+router.use(protectRoute);
+
+router.get("/users", getUsersForSidebar);
+router.get("/conversations", getConversationsForSidebar);
+router.get("/:id", getMessages);
+router.post("/send/:id", upload, sendMessages)
+// todo: show this in frontend
+
+export default router;
